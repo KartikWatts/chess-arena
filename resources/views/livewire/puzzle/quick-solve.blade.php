a65154c8ac3560ba3ffcbb866e322154b8d97fcc
KartikWatts <arorakartik4@gmail.com>
2023-04-10 10:29:02 +0530

    responsive styling of the board taken care of

--- resources/views/livewire/puzzle/quick-solve.blade.php
+++ resources/views/livewire/puzzle/quick-solve.blade.php
@@ -1,7 +1,8 @@
-<div class="flex flex-col items-center justify-center gap-2 md:gap-4 m-10">
-    {{ $puzzle }}
-    {{ $puzzle->FEN }}
+<div class="flex flex-col items-center justify-center gap-2 md:gap-4 my-10 mx-4">
     <input name="puzzleFEN" id="puzzleFEN" value="{{ $puzzle->FEN }}" hidden />
     <h1 class="text-xl md:text-3xl"><span class="font-bold">Solve Puzzle</h1>
     <div id="board" class="board"></div>
+    {{ $puzzle }}
+    {{ $puzzle->FEN }}
+
 </div>
