00ed51ae52aa5f3bfa517b30608ac380cbb0818b
KartikWatts <arorakartik4@gmail.com>
2023-04-10 19:04:20 +0530

    dynamically loading board and first move by cpu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,42 @@
 import "./bootstrap";
 
-let startPosition = document.getElementById("puzzleFEN");
-// console.log(startPosition);
-let chessBoard = Chessboard("board", "start");
-if (startPosition) chessBoard = Chessboard("board", startPosition.value);
+let startPositionFEN = document.getElementById("puzzleFEN");
+let movesList = document.getElementById("puzzleMoves");
+let moveOrder;
+
+let positionConfig = "start";
+let orientationConfig = "white";
+
+if (startPositionFEN) {
+    const [position, turn, castling, enPassant, halfMoveClock, fullMoveNumber] =
+        startPositionFEN.value.split(" ");
+    positionConfig = position;
+    // Setting Inverse Orientation as first move in the solution is to be moved by itself after load
+    orientationConfig = turn === "w" ? "black" : "white";
+}
+
+let config = {
+    position: positionConfig,
+    orientation: orientationConfig,
+    draggable: true,
+    appearSpeed: "slow",
+    moveSpeed: "slow",
+    showErrors: "alert",
+};
+
+let board = Chessboard("board", config);
+
+const getMoveForBoard = (moveString) => {
+    const part1 = moveString.slice(0, 2);
+    const part2 = moveString.slice(2);
+    return `${part1}-${part2}`;
+};
+
+if (movesList) {
+    moveOrder = movesList.value.split(" ");
+    setTimeout(() => {
+        board.move(getMoveForBoard(moveOrder[0]));
+    }, 1000);
+}
+
+$(window).resize(board.resize);
--- resources/views/livewire/puzzle/quick-solve.blade.php
+++ resources/views/livewire/puzzle/quick-solve.blade.php
@@ -1,8 +1,8 @@
 <div class="flex flex-col items-center justify-center gap-2 md:gap-4 my-10 mx-4">
     <input name="puzzleFEN" id="puzzleFEN" value="{{ $puzzle->FEN }}" hidden />
+    <input name="puzzleMoves" id="puzzleMoves" value="{{ $puzzle->Moves }}" hidden />
     <h1 class="text-xl md:text-3xl"><span class="font-bold">Solve Puzzle</h1>
     <div id="board" class="board"></div>
     {{ $puzzle }}
     {{ $puzzle->FEN }}
-
 </div>
