64ed9e8b4f7dc632dbee95ad01de2a915bd0b243
KartikWatts <arorakartik4@gmail.com>
2023-04-10 23:36:26 +0530

    implementing possible moves indicator and move highlighter

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,15 @@ import { Chess } from "chess.js";
 import "./bootstrap";
 
 var board = null;
+var $board = $("#board");
 var game = new Chess();
+var whiteSquareGrey = "#a9a9a9";
+var blackSquareGrey = "#696969";
+var squareToHighlight = null;
+var colorToHighlight = null;
+var squareClass = "square-55d63";
 
+let boardElement = document.getElementById("board");
 let startPositionFEN = document.getElementById("puzzleFEN");
 let movesList = document.getElementById("puzzleMoves");
 let movesColorElement = document.getElementById("moveColor");
@@ -41,22 +48,63 @@ function onDragStart(source, piece, position, orientation) {
 }
 
 function onDrop(source, target) {
+    removeGreySquares();
+
     // see if the move is legal
-    var move = game.move({
-        from: source,
-        to: target,
+    try {
+        var move = game.move({
+            from: source,
+            to: target,
+        });
+        if (move.color === "w") {
+            $board.find("." + squareClass).removeClass("highlight-white");
+            $board.find(".square-" + move.from).addClass("highlight-white");
+            squareToHighlight = move.to;
+            colorToHighlight = "white";
+        } else {
+            $board.find("." + squareClass).removeClass("highlight-black");
+            $board.find(".square-" + move.from).addClass("highlight-black");
+            squareToHighlight = move.to;
+            colorToHighlight = "black";
+        }
+        console.log(move);
+    } catch (error) {
+        return "snapback";
+    }
+    updateStatus();
+}
+
+function onMouseoverSquare(square, piece) {
+    // get list of possible moves for this square
+    var moves = game.moves({
+        square: square,
+        verbose: true,
     });
 
-    console.log(move);
+    // exit if there are no moves available for this square
+    if (moves.length === 0) return;
 
-    // illegal move
-    if (move === null) return "snapback";
+    // highlight the square they moused over
+    greySquare(square);
 
-    updateStatus();
+    // highlight the possible squares for this piece
+    for (var i = 0; i < moves.length; i++) {
+        greySquare(moves[i].to);
+    }
+}
+
+function onMouseoutSquare(square, piece) {
+    removeGreySquares();
 }
 
 function onSnapEnd() {
     board.position(game.fen());
+}
+
+function onMoveEnd() {
+    $board
+        .find(".square-" + squareToHighlight)
+        .addClass("highlight-" + colorToHighlight);
 }
 
 function getStrippedPGN(pgnString) {
@@ -95,6 +143,10 @@ function updateStatus() {
     $fen.html(game.fen());
     $pgn.html(getStrippedPGN(game.pgn()));
     console.log(game.ascii());
+
+    solutionMoveIndex++;
+    if (solutionMoveIndex % 2 == 0)
+        boardElement.classList.toggle("disable-actions");
 }
 
 let config = {
@@ -106,8 +158,26 @@ let config = {
     showErrors: "alert",
     onDragStart: onDragStart,
     onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
     onSnapEnd: onSnapEnd,
+    onMoveEnd: onMoveEnd,
 };
+
+function removeGreySquares() {
+    $("#board .square-55d63").css("background", "");
+}
+
+function greySquare(square) {
+    var $square = $("#board .square-" + square);
+
+    var background = whiteSquareGrey;
+    if ($square.hasClass("black-3c85d")) {
+        background = blackSquareGrey;
+    }
+
+    $square.css("background", background);
+}
 
 board = Chessboard("board", config);
 
