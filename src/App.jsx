7aa136a0b070083e8efcce754f2e910a29a7d6d1
KartikWatts <arorakartik4@gmail.com>
2024-06-30 17:59:10 +0530

    basic vite react project setup with chess libraries
    using `chess.js` and `react-chessboard`

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { Chessboard } from "react-chessboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center gap-2 md:gap-4 m-10'>
+      <h1 className='text-3xl md:text-5xl'>
+        <span className='font-bold'>Chess Arena:</span> A Chess Puzzle Platform
+      </h1>
+      <div className='board'>
+        <Chessboard
+          id='Configurable Board'
+          onArrowsChange={function noRefCheck() {}}
+          onDragOverSquare={function noRefCheck() {}}
+          onMouseOutSquare={function noRefCheck() {}}
+          onMouseOverSquare={function noRefCheck() {}}
+          onPieceClick={function noRefCheck() {}}
+          onPieceDragBegin={function noRefCheck() {}}
+          onPieceDragEnd={function noRefCheck() {}}
+          onPieceDrop={function noRefCheck() {}}
+          onPromotionCheck={function noRefCheck() {}}
+          onPromotionPieceSelect={function noRefCheck() {}}
+          onSquareClick={function noRefCheck() {}}
+          onSquareRightClick={function noRefCheck() {}}
+        />
+      </div>
+      <div className='app-main-content'>
+        <a href="{{ route('test') }}">
+          <div className='bg-slate-900 p-4 md:p-10 rounded-xl md:rounded-3xl text-xl md:text-4xl hover:bg-slate-800 hover:scale-110 motion-safe:animate-pulse solve-now-btn cursor-pointer'>
+            Solve Now
+          </div>
+        </a>
+        <div className='bg-slate-900 py-3 px-5 mt-5 rounded-md beta-release-caution'>
+          The app is in beta mode, as it may still contain undiscovered bugs and
+          unexpected behavior. I appreciate providing feedback on any issues
+          encountered, so we can improve the app before its official release.
+          Contributions are dearly welcomed. Made with ❤️{" "}
+          <a href='https://github.com/KartikWatts/chess-arena' target='_blank'>
+            https://github.com/KartikWatts/chess-arena
+          </a>
+          <br /> Note: For now in Mobile browser, moves can only be played by
+          dragging the piece.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
